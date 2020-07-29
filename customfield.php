a910180832814c51ddded7dc083bb03413b06b4d
Paul McCormick <paul@effigy.com.au>
2020-07-29 23:12:07 +1200

    fix config include url

--- customfield.php
+++ customfield.php
@@ -20,7 +20,7 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once('../../../config.php');
+require_once('../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 
 admin_externalpage_setup('forum_customfield');
